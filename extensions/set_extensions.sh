177d6129e080ea0560a3398279fda8bb78e248ef
KroJIak <rastoandrei@gmail.com>
2024-09-20 00:59:00 +0300

    moved extensions script to scripts folder

--- extensions/set_extensions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo rm -r ~/.local/share/gnome-shell/extensions
-cp -r local ~/.local/share/gnome-shell/extensions
-sudo rm -r /usr/share/gnome-shell/extensions
-sudo cp -r usr /usr/share/gnome-shell/extensions
