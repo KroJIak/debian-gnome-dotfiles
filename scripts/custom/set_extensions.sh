00b09280ee6f5d66b0b2d0a72b3c3838a7cba1ed
KroJIak <rastoandrei@gmail.com>
2024-09-24 15:21:37 +0300

    updated set_extensions script

--- scripts/custom/set_extensions.sh
+++ scripts/custom/set_extensions.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo rm -r ~/.local/share/gnome-shell/extensions
-cp -r ../../extensions/local ~/.local/share/gnome-shell/extensions
-sudo rm -r /usr/share/gnome-shell/extensions
-sudo cp -r ../../extensions/usr /usr/share/gnome-shell/extensions
+cp -r ../../extensions/backup/* ~/.local/share/gnome-shell/extensions/
+dconf load /org/gnome/shell/extensions/ < ../../extensions/settings_backup.txt
