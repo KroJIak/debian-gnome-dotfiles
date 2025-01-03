49152b3cc3e5f9f7343c3f596bca8a0a1845ebd8
Andrey Rastopshin <61227521+KroJIak@users.noreply.github.com>
2025-01-03 17:48:45 +0300

    Update required_apps.sh

--- scripts/apps/required_apps.sh
+++ scripts/apps/required_apps.sh
@@ -5,6 +5,6 @@ sudo apt install -y dconf-editor fish gnome-pie grub-customizer kitty pulseaudio
 
 
 # Change the default shell to fish
-chsh $USER -s /usr/bin/fish
+sudo chsh $USER -s /usr/bin/fish
 
 sudo apt remove gnome-screenshot && sudo apt install -y flameshot
